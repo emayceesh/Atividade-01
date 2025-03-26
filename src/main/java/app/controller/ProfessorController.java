1dc4343270cc064430c30fa91e898f14312f4daa
Patryk Rennan <165728048+emayceesh@users.noreply.github.com>
2025-03-26 20:31:03 -0300

    commit integração

--- src/main/java/app/controller/ProfessorController.java
+++ src/main/java/app/controller/ProfessorController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import app.service.ProfessorService;
 
 @RestController
 @RequestMapping("/api/professor")
+@CrossOrigin("*")
 public class ProfessorController {
 
 	@Autowired
